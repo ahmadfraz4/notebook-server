d15bb40ed62533570cdf01659199f1734d772912
ahmadfraz4 <ahmadfraz2015@gmail.com>
2023-11-19 20:43:10 +0500

    redirect

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -4,9 +4,10 @@ let jwt = require('jsonwebtoken')
 let authenticate = async (req,res,next) =>{
     try {
         let token = req.cookies.jwtToken;
-        // if(!token){
-        //     return res.json({msg : 'Please Enable the cookies to access notes', msgType : 'error'})
-        // }
+        if(!token){
+            res.redirect(`${process.env.FRONTEND_PATH}/login`);
+            return res.json({msg : 'Please Enable the cookies to access notes', msgType : 'error'})
+        }
         let verifyToken = jwt.verify(token, process.env.SECRET_KEY)
         let rootUser = await User.findOne({_id:verifyToken._id, "tokens.token" : token})
         if(!rootUser){
