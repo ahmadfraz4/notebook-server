8a4cf6ebc2189316d082902add1f1019c840f086
ahmadfraz4 <ahmadfraz2015@gmail.com>
2023-11-19 20:37:51 +0500

    token

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -4,9 +4,9 @@ let jwt = require('jsonwebtoken')
 let authenticate = async (req,res,next) =>{
     try {
         let token = req.cookies.jwtToken;
-        if(!token){
-            return res.json({msg : 'Please Enable the cookies to access notes', msgType : 'error'})
-        }
+        // if(!token){
+        //     return res.json({msg : 'Please Enable the cookies to access notes', msgType : 'error'})
+        // }
         let verifyToken = jwt.verify(token, process.env.SECRET_KEY)
         let rootUser = await User.findOne({_id:verifyToken._id, "tokens.token" : token})
         if(!rootUser){
